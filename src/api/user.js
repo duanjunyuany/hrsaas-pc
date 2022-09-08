97ce9622652b34dbba7194f2cf475124b00a687b
duanjunyuany <3164005225@qq.com>
2022-09-08 18:44:21 +0800

    api模块封装

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,19 @@
 import request from '@/utils/request'
 
+// 登录
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
   })
 }
 
+// 获取信息
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
   })
 }
 
+// 退出
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
   })
 }
