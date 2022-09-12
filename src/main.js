b3d06ddc2efbf897229869a02ab6f872d17900c5
duanjunyuany <3164005225@qq.com>
2022-09-12 14:22:02 +0800

    员工列表数据格式化

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import App from './App'
 import store from './store'
 import router from './router'
 import Component from '@/components'
+import * as filters from '@/filters'
 
 import '@/icons'
 import '@/permission'
@@ -19,6 +20,11 @@ import '@/permission'
 Vue.use(ElementUI, { locale })
 // 注册自定义组件
 Vue.use(Component)
+// 注册全局的过滤器
+Object.keys(filters).forEach(key => {
+  // 注册过滤器
+  Vue.filter(key, filters[key])
+})
 
 Vue.config.productionTip = false
 
